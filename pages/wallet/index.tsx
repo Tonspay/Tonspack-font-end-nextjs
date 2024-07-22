dcae834dd6f84dde53568a85808cae4f51f4ac27
Your Name <you@example.com>
2024-07-22 06:30:08 +0000

    now try add request

--- pages/wallet/index.tsx
+++ pages/wallet/index.tsx
@@ -9,17 +9,17 @@ export default function DocsPage() {
   const list = [
     {
       title: "TON",
-      address:"EQCZBjHIQsKYDo4xob7C3IbHL8X4hUz1Q4A7BgzXNNt_yPIP",
+      address:"EQCZBjHI...NNt_yPIP",
       img: "/images/chains/ton.jpg",
     },
     {
       title: "ETH",
-      address:"0xF1a4db7963afB51ef0465BaA805914A2ACF951f9",
+      address:"0xF1a4db...ACF951f9",
       img: "/images/chains/eth.jpg",
     },
     {
       title: "Solana",
-      address:"1YF4hSrqRqSKwnLDwkR1df5chDEnAV67aBL2EKYXzXS",
+      address:"1YF4hSrq...2EKYXzXS",
       img: "/images/chains/solana.jpg",
     },
   ];
@@ -62,14 +62,14 @@ export default function DocsPage() {
       <Image
         alt="Woman listing to music"
         className="object-cover"
-        height={250}
+        height={200}
         src={item.img}
-        width={500}
+        width={400}
       />
 
       
       <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-        <p className="text-tiny text-white/80">{item.address}</p>
+        <p className="text-tiny text-white/80" style={{"font-size":"10px"}}>{item.address}</p>
         <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
           Copy
         </Button>
