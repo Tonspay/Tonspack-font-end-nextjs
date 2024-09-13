9cdf85752276e804eb0dd37f1e4e41712f66d07d
JackChanel <1667716062@qq.com>
2024-09-13 15:15:04 +0800

    feat: setting page ui

---
+++ pages/setting/index copy.tsx
@@ -0,0 +1,111 @@
+import { Link } from "@nextui-org/link";
+import { Button } from "@nextui-org/button";
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
+import { Divider } from "@nextui-org/divider";
+import { useState, useEffect } from "react";
+import { Toaster } from "react-hot-toast";
+
+import { deving, exportPrivateKey } from "../../core/utils/utils";
+import { telegramShareApp } from "../../core/utils/tg";
+
+import { Loading } from "@/components/loading";
+import DefaultLayout from "@/layouts/default";
+import { title } from "@/components/primitives";
+
+type walletCard = {
+  title: string;
+  address: string;
+  scan: string;
+  img: string;
+  name: string;
+  bal: string;
+};
+
+export default function DocsPage() {
+  // let list : walletCard[];
+  let list: walletCard[];
+
+  list = [];
+  const [data, setData] = useState([
+    {
+      title: "pending",
+      address: "",
+      scan: "",
+      img: "",
+      name: "",
+      bal: "",
+    },
+  ]);
+
+  const [isMainPageLoading, setIsMainPageLoading] = useState(true);
+
+  const [isNav, setIsNav] = useState("");
+
+  useEffect(() => {
+    const onload = async () => {
+      setIsMainPageLoading(false);
+      setIsNav("setting");
+    };
+
+    onload().catch(console.error);
+  }, []);
+
+  return (
+    <DefaultLayout name={isNav}>
+      {isMainPageLoading ? <Loading /> : null}
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <Toaster />
+
+        <div className="inline-block max-w-lg text-center justify-center">
+          <h1 className={title()}>Setting</h1>
+        </div>
+
+        <div style={{ maxWidth: "400px", width: "100%" }}>
+          <Card className="max-w-[400px]">
+            <CardHeader
+              className="flex gap-3"
+              style={{ textAlign: "center", maxWidth: "400px", width: "100%" }}
+            >
+              <div style={{ textAlign: "center", width: "100%" }}>
+                <p className="text-md">Set up your wallet</p>
+              </div>
+            </CardHeader>
+            <Divider style={{ maxWidth: "400px", width: "100%" }} />
+            <CardBody>
+              <br />
+              <Button
+                color="primary"
+                variant="bordered"
+                onClick={exportPrivateKey}
+              >
+                Export privateKey
+              </Button>
+              <br />
+              <Button
+                color="secondary"
+                variant="bordered"
+                onClick={telegramShareApp}
+              >
+                Share Tonspack
+              </Button>
+              <br />
+              <Button color="warning" variant="bordered" onClick={deving}>
+                Share Profite
+              </Button>
+              <br />
+              <Button color="danger" variant="bordered" onClick={deving}>
+                Close
+              </Button>
+            </CardBody>
+            <Divider />
+            <CardFooter>
+              <Link isExternal showAnchorIcon href="https://t.me/Tonspackdev">
+                Join tonspack community for support.
+              </Link>
+            </CardFooter>
+          </Card>
+        </div>
+      </section>
+    </DefaultLayout>
+  );
+}
--- pages/setting/index.tsx
+++ pages/setting/index.tsx
@@ -1,16 +1,10 @@
-import { Link } from "@nextui-org/link";
-import { Button } from "@nextui-org/button";
-import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
-import { Divider } from "@nextui-org/divider";
 import { useState, useEffect } from "react";
 import { Toaster } from "react-hot-toast";
-
-import { deving, exportPrivateKey } from "../../core/utils/utils";
-import { telegramShareApp } from "../../core/utils/tg";
+import { Image } from "@nextui-org/image";
+import { IoIosArrowForward } from "react-icons/io";
 
 import { Loading } from "@/components/loading";
 import DefaultLayout from "@/layouts/default";
-import { title } from "@/components/primitives";
 
 type walletCard = {
   title: string;
@@ -22,21 +16,6 @@ type walletCard = {
 };
 
 export default function DocsPage() {
-  // let list : walletCard[];
-  let list: walletCard[];
-
-  list = [];
-  const [data, setData] = useState([
-    {
-      title: "pending",
-      address: "",
-      scan: "",
-      img: "",
-      name: "",
-      bal: "",
-    },
-  ]);
-
   const [isMainPageLoading, setIsMainPageLoading] = useState(true);
 
   const [isNav, setIsNav] = useState("");
@@ -50,62 +29,96 @@ export default function DocsPage() {
     onload().catch(console.error);
   }, []);
 
-  // return returnFont()
   return (
     <DefaultLayout name={isNav}>
       {isMainPageLoading ? <Loading /> : null}
-      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <section className="flex flex-col  justify-centerpy-8 md:py-10 bg-gray-200">
         <Toaster />
-
-        <div className="inline-block max-w-lg text-center justify-center">
-          <h1 className={title()}>Setting</h1>
+        {/* Header */}
+        <div className="flex justify-start items-center gap-4">
+          <Image
+            alt="chain logo"
+            height={50}
+            src="/images/chains/btc.svg"
+            width={50}
+          />
+          <div>
+            <p className="text-black font-semibold">Guarda Wallet</p>
+            <p className="text-gray-500 text-xs">Version 3.0.73-build:4</p>
+          </div>
+        </div>
+        {/* Setting Card1 */}
+        <div className="flex bg-white rounded-md p-3 mt-4">
+          <div className="min-w-10">
+            <Image
+              alt="chain logo"
+              height={45}
+              src="/images/chains/btc.svg"
+              width={45}
+            />
+          </div>
+          <div className="ml-2">
+            <p className="text-black">Backup</p>
+            <p className="text-gray-500 text-xs mt-1">
+              Keep a copy of your wallet in a safe place and never give it to
+              others. Anyone who gets hold of your Backup file will be able to
+              access your coins
+            </p>
+          </div>
+        </div>
+        {/* Setting Card2 */}
+        <div className="flex bg-white rounded-md p-3 mt-2">
+          <div className="min-w-10">
+            <Image
+              alt="chain logo"
+              height={45}
+              src="/images/chains/btc.svg"
+              width={45}
+            />
+          </div>
+          <div className="ml-2">
+            <p className="text-black">Import</p>
+            <p className="text-gray-500 text-xs mt-1">
+              Import your wallet using Private Key, Mnemonic, WIF or XPRV
+            </p>
+          </div>
         </div>
 
-        <div style={{ maxWidth: "400px", width: "100%" }}>
-          <Card className="max-w-[400px]">
-            <CardHeader
-              className="flex gap-3"
-              style={{ textAlign: "center", maxWidth: "400px", width: "100%" }}
-            >
-              <div style={{ textAlign: "center", width: "100%" }}>
-                <p className="text-md">Set up your wallet</p>
-              </div>
-            </CardHeader>
-            <Divider style={{ maxWidth: "400px", width: "100%" }} />
-            <CardBody>
-              <br />
-              <Button
-                color="primary"
-                variant="bordered"
-                onClick={exportPrivateKey}
-              >
-                Export privateKey
-              </Button>
-              <br />
-              <Button
-                color="secondary"
-                variant="bordered"
-                onClick={telegramShareApp}
-              >
-                Share Tonspack
-              </Button>
-              <br />
-              <Button color="warning" variant="bordered" onClick={deving}>
-                Share Profite
-              </Button>
-              <br />
-              <Button color="danger" variant="bordered" onClick={deving}>
-                Close
-              </Button>
-            </CardBody>
-            <Divider />
-            <CardFooter>
-              <Link isExternal showAnchorIcon href="https://t.me/Tonspackdev">
-                Join tonspack community for support.
-              </Link>
-            </CardFooter>
-          </Card>
+        {/* Setting Column2 */}
+        <h2 className="text-gray-500 text-sm mt-8">SERVICE</h2>
+        <div className="flex bg-white rounded-md p-3 mt-2">
+          <div className="min-w-10">
+            <Image
+              alt="chain logo"
+              height={45}
+              src="/images/chains/btc.svg"
+              width={45}
+            />
+          </div>
+          <div className="ml-2 grow flex justify-between items-center">
+            <p className="text-black text-sm">WalletConnect</p>
+            <p className="text-xxs text-gray-500">Will be live soon</p>
+          </div>
         </div>
+        {/* Setting Column3 */}
+
+        <h2 className="text-gray-500 text-sm mt-8">Settings</h2>
+        {[1, 2, 3].map((item, index) => (
+          <div key={index} className="flex bg-white rounded-md p-3 mt-2">
+            <div className="min-w-10">
+              <Image
+                alt="chain logo"
+                height={45}
+                src="/images/chains/btc.svg"
+                width={45}
+              />
+            </div>
+            <div className="ml-2 grow flex justify-between items-center">
+              <p className="text-black text-sm">WalletConnect</p>
+              <IoIosArrowForward className="text-gray-500" />
+            </div>
+          </div>
+        ))}
       </section>
     </DefaultLayout>
   );
